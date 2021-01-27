8da66c67cb37390c0ff353659e929ae37e755e85
waiscodes <b.wais@outlook.com>
2021-01-27 13:23:34 -0700

    moved code into seperate component

---
+++ src/components/DisplayListVideos.vue
@@ -0,0 +1,29 @@
+<template>
+  <ul>
+    <li v-for="item in itemsArray" :key="item">
+      {{ item }}
+    </li>
+  </ul>
+</template>
+
+<script>
+import db from "../db.js";
+
+export default {
+  data() {
+    return {
+      itemsArray: [],
+    };
+  },
+  methods: {},
+  mounted() {
+    db.collection("videos").onSnapshot((querySnapshot) => {
+      const items = querySnapshot.docs.map((doc) => {
+        return doc.data();
+      });
+      this.itemsArray.push(...items);
+    });
+  },
+};
+</script>
+<style scoped=""></style>
