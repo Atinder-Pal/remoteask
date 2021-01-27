680f3399699d6070286b76c639e11f364a4d96ec
waiscodes <b.wais@outlook.com>
2021-01-27 13:28:44 -0700

    added the placeholder text for video display

--- src/components/DisplayListVideos.vue
+++ src/components/DisplayListVideos.vue
@@ -1,4 +1,10 @@
 <template>
+  <section class="display">
+    <iframe src="" frameborder="0"></iframe>
+    <h3>Title</h3>
+    <p>Topic</p>
+    <p>Time Stamp</p>
+  </section>
   <ul>
     <li v-for="item in itemsArray" :key="item">
       {{ item }}
@@ -12,6 +18,7 @@ import db from "../db.js";
 export default {
   data() {
     return {
+      selectedItem: null,
       itemsArray: [],
     };
   },
