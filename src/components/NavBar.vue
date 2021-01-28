fc4e5bb88737b5affaf3226e893e986a9a9814c8
Danish <danishriz100@gmail.comgit commitexit>
2021-01-28 13:46:33 -0700

    added navbar and signup button in navbar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,52 @@
+<template>
+ 
+ 
+
+
+
+
+   <ion-toolbar>
+
+
+
+        <ion-title>RemoteAsk</ion-title>
+        <router-link to="/signup">
+           <ion-button fill="outline">Sign Up</ion-button>
+        </router-link>
+
+
+      </ion-toolbar>
+
+
+</template>
+
+<script>
+import {
+//   IonHeader,
+//   IonPage,
+//   IonTitle,
+//   IonToolbar,
+//  IonButton,
+} from "@ionic/vue";
+import { defineComponent } from "vue";
+
+
+export default defineComponent({
+  name: "LogoutTemplate",
+  components: {
+    // IonHeader,
+    // IonPage,
+    // IonTitle,
+    // IonToolbar,
+  //  IonButton,
+  },
+
+  methods: {
+
+  },
+});
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,12 +1,8 @@
 <template>
   <ion-page>
     <ion-header :translucent="true">
-      <ion-toolbar>
-        <ion-title>Welcome to Login Page</ion-title>
-        <router-link to="/home">
-          <ion-button>HOME</ion-button>
-        </router-link>
-      </ion-toolbar>
+ 
+ <NavBar />
     </ion-header>
 
     <ion-content :fullscreen="true">
@@ -61,8 +57,8 @@ import {
   IonContent,
   IonHeader,
   IonPage,
-  IonTitle,
-  IonToolbar,
+  // IonTitle,
+  // IonToolbar,
   IonButton,
   IonInput,
 } from "@ionic/vue";
@@ -71,16 +67,19 @@ import firebase from "firebase/app";
 // eslint-disable-next-line no-unused-vars
 import { db } from "../db";
 
+import NavBar from "../components/NavBar";
+
 export default defineComponent({
   name: "Login",
   components: {
     IonContent,
     IonHeader,
     IonPage,
-    IonTitle,
-    IonToolbar,
+    // IonTitle,
+    // IonToolbar,
     IonButton,
     IonInput,
+    NavBar,
   },
   data() {
     return {
