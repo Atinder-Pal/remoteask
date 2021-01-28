2507a69e67069b1279ffbba71d956151b8afbfb9
Danish <danishriz100@gmail.comgit commitexit>
2021-01-28 12:21:07 -0700

    structure login page

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -3,70 +3,76 @@
     <ion-header :translucent="true">
       <ion-toolbar>
         <ion-title>Welcome to Login Page</ion-title>
-          <router-link to="/home">
-        <ion-button>HOME</ion-button>
+        <router-link to="/home">
+          <ion-button>HOME</ion-button>
         </router-link>
-   
       </ion-toolbar>
     </ion-header>
-    
+
     <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Login Page</ion-title>
-        </ion-toolbar>
-      </ion-header>
-    
+      <ion-card class="center-ra">
+        <ion-card-header>
+          <h1>SignIn to Your RemoteAsk account</h1>
+        </ion-card-header>
+
+        <ion-card-content>
+          <form @submit.prevent="userLogin">
+            <ion-list>
+              <ion-item>
+                <ion-label stacked>Email</ion-label>
+                <ion-input
+                  type="email"
+                  v-model="user.email"
+                  placeholder="Enter email"
+                ></ion-input>
+              </ion-item>
+
+              <ion-item>
+                <ion-label stacked>Password</ion-label>
+                <ion-input
+                  type="password"
+                  v-model="user.password"
+                  placeholder="Enter Password"
+                ></ion-input>
+              </ion-item>
+            </ion-list>
+
+            <ion-button expand="block" type="submit" class="margin-ra"> Sign In </ion-button>
+          </form>
+
+          <ion-button expand="block" fill="outline" class="margin-ra" @click="googleLogin"
+            >Sign In With Google</ion-button
+          >
+        </ion-card-content>
+
+        <ion-card-footer>
+          Dont have an account yet click here to
+          <router-link to="/signup">Sign Up</router-link>
+        </ion-card-footer>
+      </ion-card>
 
 
-      <div>
-      <p>Dont have an account yet click here to
-                   <router-link to="/signup"
-              >Sign up</router-link ></p>
-        </div>
-
-   <form @submit.prevent="userLogin">
-
-  <h1>SignIn to Your RemoteAsk account</h1>
-
-      <div>
-        <label>Email</label>
-        <ion-input
-          type="email"
-          v-model="user.email"
-        placeholder="Enter email"></ion-input>
-      </div>
-
-      <div>
-        <label>Password</label>
-        <ion-input
-          type="password"
-          v-model="user.password"
-        placeholder="Enter Password"></ion-input>
-      </div>
-
-      <ion-button type="submit">
-        Sign In
-      </ion-button>
-
-
-     </form>
-
-
-   <ion-button @click="googleLogin">SignIn With Google</ion-button>
     </ion-content>
   </ion-page>
 </template>
 
 <script>
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton, IonInput } from '@ionic/vue';
-import { defineComponent } from 'vue';
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonButton,
+  IonInput,
+} from "@ionic/vue";
+import { defineComponent } from "vue";
 import firebase from "firebase/app";
 // eslint-disable-next-line no-unused-vars
-import { db } from '../db'
+import { db } from "../db";
 
 export default defineComponent({
-  name: 'Login',
+  name: "Login",
   components: {
     IonContent,
     IonHeader,
@@ -74,14 +80,14 @@ export default defineComponent({
     IonTitle,
     IonToolbar,
     IonButton,
-    IonInput
+    IonInput,
   },
- data() {
+  data() {
     return {
       user: {
         email: "",
-        password: ""
-      }
+        password: "",
+      },
     };
   },
   methods: {
@@ -90,59 +96,43 @@ export default defineComponent({
         .auth()
         .signInWithEmailAndPassword(this.user.email, this.user.password)
         // eslint-disable-next-line no-unused-vars
-        .then(data => {
-         // this.$router.push("/signedin");
+        .then((data) => {
+          // this.$router.push("/signedin");
           this.$router.replace({ name: "Signedin" });
         })
-        .catch(error => {
+        .catch((error) => {
           alert(error.message);
         });
     },
-    googleLogin(){
-       // Firebase Google Sign-in
+    googleLogin() {
+      // Firebase Google Sign-in
 
-    const provider = new firebase.auth.GoogleAuthProvider();
-        // eslint-disable-next-line no-unused-vars
-        firebase.auth().signInWithPopup(provider).then((result) => {
-          this.$router.replace('Signedin');
-        }).catch((err) => {
-          alert('Oops. ' + err.message)
+      const provider = new firebase.auth.GoogleAuthProvider();
+     
+      firebase
+        .auth()
+        .signInWithPopup(provider)
+         // eslint-disable-next-line no-unused-vars
+        .then((result) => {
+          this.$router.replace("Signedin");
+        })
+        .catch((err) => {
+          alert("Oops. " + err.message);
         });
-    
     },
-  }
-
-
-
+  },
 });
 </script>
 
-<style scoped>
-#container {
-  text-align: center;
-  
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 15%;
-  transform: translateY(-50%);
+<style>
+.margin-ra {
+  margin: 20px;
 }
 
-#container strong {
-  font-size: 20px;
-  line-height: 26px;
+.center-ra {
+  max-width: 400px;
+  margin-right: auto;
+  margin-left:auto;
 }
 
-#container p {
-  font-size: 16px;
-  line-height: 22px;
-  
-  color: #8c8c8c;
-  
-  margin: 0;
-}
-
-#container a {
-  text-decoration: none;
-}
 </style>
