a2b0e5888cda34db0467ffcd34e970b6b49d682e
waiscodes <b.wais@outlook.com>
2021-01-28 16:34:09 -0700

    added files over manually

--- src/components/DisplayListVideos.vue
+++ src/components/DisplayListVideos.vue
@@ -1,9 +1,9 @@
 <template>
   <section class="display">
-    <iframe :src="selectedItem.link" frameborder="0"></iframe>
-    <h3>{{ selectedItem.title }}</h3>
-    <p>{{ selectedItem.topic }}</p>
-    <p>{{ selectedItem.createdAt }}</p>
+    <iframe id="video-frame" :src="selectedItem.link" frameborder="0"></iframe>
+    <h3 id="video-title">{{ selectedItem.title }}</h3>
+    <p id="video-topic">{{ selectedItem.topic }}</p>
+    <p id="video-timestamp">{{ selectedItem.createdAt }}</p>
   </section>
   <ul class="videosList">
     <li
@@ -11,20 +11,20 @@
       :key="item"
       :id="JSON.stringify(item)"
       class="listItem"
-      @click="selectVideo"
+      @click.capture="selectVideo"
     >
-      <span>{{ item.title }}</span>
-      <br />
-      <span>{{ item.topic }}</span>
-      <br />
-      <span>{{ item.createdAt.toDate().toDateString() }}</span>
-      <br /><br /><br />
+      <span class="list-title">{{ item.title }}</span>
+      <span class="list-topic">{{ item.topic }}</span>
+      <span class="list-timestamp">{{
+        item.createdAt.toDate().toDateString()
+      }}</span>
     </li>
   </ul>
 </template>
 
 <script>
 import db from "../db.js";
+import firebase from "firebase";
 
 export default {
   data() {
@@ -41,31 +41,74 @@ export default {
         topic: video.topic,
         createdAt: video.createdAt,
       };
+
+      let timestamp = this.selectedItem.createdAt; // firebase data
+      let myDate = new Date(timestamp.seconds * 1000); // date object
+      this.selectedItem.createdAt = myDate.toDateString();
     },
     selectVideo(e) {
+      event.stopPropagation();
       const video = JSON.parse(e.target.id);
-      console.log(video.createdAt);
       this.setDisplayVideo(video);
     },
   },
   beforeMount() {
-    db.collection("videos").onSnapshot((querySnapshot) => {
-      const items = querySnapshot.docs.map((doc) => {
-        return doc.data();
-      });
-      this.itemsArray.push(...items);
-      this.setDisplayVideo(items[0]);
+    firebase.auth().onAuthStateChanged((user) => {
+      db.collection("videos")
+        .where("userId", "==", user.uid)
+        .onSnapshot((querySnapshot) => {
+          const items = querySnapshot.docs.map((doc) => {
+            return doc.data();
+          });
+          this.itemsArray.push(...items);
+          this.setDisplayVideo(items[0]);
+        });
     });
   },
 };
 </script>
 <style scoped="">
+#video-frame {
+  border: solid;
+  width: 100vw;
+}
+
 .videosList {
   padding: 0px;
+}
+
+#video-title {
+  text-transform: capitalize;
+  margin: 0px;
+}
+
+#video-topic {
+  margin-bottom: 0px;
+}
+
+#video-timestamp {
+  margin: 0px;
 }
 
 .listItem {
   border: solid;
   display: block;
+  display: flex;
+  flex-direction: column;
+}
+
+.list-title {
+  font-size: 20px;
+  text-transform: capitalize;
+  width: max-content;
+  border: solid;
+}
+.list-topic {
+  width: max-content;
+  border: solid;
+}
+.list-timestamp {
+  width: max-content;
+  border: solid;
 }
 </style>
