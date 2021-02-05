8e5af4ebecc32a09100d25511ecdab744efc76f5
waiscodes <b.wais@outlook.com>
2021-02-05 08:40:21 -0700

    created a video landing page

---
+++ src/views/VideoLanding.vue
@@ -0,0 +1,42 @@
+<template>
+  <ion-page>
+    <ion-header :translucent="true">
+      <NavBar v-bind:upload="upload" />
+    </ion-header>
+    <ion-content :fullscreen="true">
+      <div id="container">
+        <VideoLandingPage />
+      </div>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script>
+import { IonContent, IonHeader, IonPage } from "@ionic/vue";
+import { defineComponent } from "vue";
+import NavBar from "../components/NavBar";
+import VideoLandingPage from "../components/VideoLandingPage";
+
+export default defineComponent({
+  name: "ListVideos",
+  components: {
+    IonContent,
+    IonHeader,
+    IonPage,
+    VideoLandingPage,
+    NavBar,
+  },
+  data() {
+    return {
+      upload: true,
+    };
+  },
+});
+</script>
+
+<style scoped>
+#container {
+  max-width: 600px;
+  margin: auto;
+}
+</style>
