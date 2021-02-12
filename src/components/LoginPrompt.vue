f03640e1eadfee1b2e1e96f2f19bdaa7926249c3
waiscodes <b.wais@outlook.com>
2021-02-11 18:42:15 -0700

    added a login and register component with router link to be added to videolanding and askquestions page

---
+++ src/components/LoginPrompt.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <router-link to="/login">
+      <button>Login</button>
+    </router-link>
+    <router-link to="/signup">
+      <button>Register</button>
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LoginPrompt",
+};
+</script>
