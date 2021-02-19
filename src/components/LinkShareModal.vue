0e8000eae718858ca6f680ce3a86b95c67d068af
Atinder-Pal <atty.pal46@gmail.com>
2021-02-19 06:52:19 -0700

    Created LinkShareModal component; Added it in AnswerQuestion.vue to show modal on clicking Share Question button;

--- src/components/LinkShareModal.vue
+++ src/components/LinkShareModal.vue
@@ -49,16 +49,16 @@
  </script>
 
 <style scoped>
-    .modal-mask {
-  position: fixed;
-  z-index: 9998;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0, 0, 0, 0.5);
-  display: table;
-  transition: opacity 0.3s ease;
+  .modal-mask {
+    position: fixed;
+    z-index: 9998;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5);
+    display: table;
+    transition: opacity 0.3s ease;
 }
 
 .modal-wrapper {
